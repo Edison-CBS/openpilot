159085b3b4899d998d9d3f5b9666b32c954d9e13
Edison <chen.poshu@gmail.com>
2022-05-01 09:59:02 +0800

    LQR still be better now
    fix bad indentation
    
    selfdrive/controls/lib/tests/test_latcontrol.py
    >> no revert to lqr version, cause RAV4 keep using LatControlTorque model
    
    fix update fun with extra llk param
    
    fix again

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -60,7 +60,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerRatio = 18.1
       tire_stiffness_factor = 0.5533
       ret.mass = 3681. * CV.LB_TO_KG + STD_CARGO_KG
-      set_lat_tune(ret.lateralTuning, LatTunes.TORQUE, MAX_LAT_ACCEL=2.0, FRICTION=0.038)
+      set_lat_tune(ret.lateralTuning, LatTunes.LQR_PV)
 
     elif candidate in (CAR.RAV4, CAR.RAV4H):
       stop_and_go = True if (candidate in CAR.RAV4H) else False
