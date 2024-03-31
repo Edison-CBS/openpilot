ab6edac2edf6a289631dcd1e0c101ead3d6078d4
Edison-CBS <chen.poshu@gmail.com>
2024-03-31 09:01:33 +0800

    toyota: remove GearShifter support
    algin

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -10,7 +10,7 @@ from openpilot.selfdrive.car.interfaces import CarInterfaceBase
 ButtonType = car.CarState.ButtonEvent.Type
 EventName = car.CarEvent.EventName
 SteerControlType = car.CarParams.SteerControlType
-GearShifter = car.CarState.GearShifter
+
 
 class CarInterface(CarInterfaceBase):
   @staticmethod
@@ -176,7 +176,7 @@ class CarInterface(CarInterfaceBase):
       ret.buttonEvents = create_button_events(self.CS.distance_button, self.CS.prev_distance_button, {1: ButtonType.gapAdjustCruise})
 
     # events
-    events = self.create_common_events(ret, extra_gears=[GearShifter.sport])
+    events = self.create_common_events(ret)
 
     # Lane Tracing Assist control is unavailable (EPS_STATUS->LTA_STATE=0) until
     # the more accurate angle sensor signal is initialized
