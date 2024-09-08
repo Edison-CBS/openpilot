87b2750fc72d0b7fe39b443ad7febf3639d427c4
Edison-CBS <chen.poshu@gmail.com>
2024-09-08 16:30:33 +0800

    bug fix: unprocessed merge selection

--- release/release_files.py
+++ release/release_files.py
@@ -120,10 +120,6 @@ whitelist = [
   "opendbc_repo/dbc/vw_mqb_2010.dbc",
 ]
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 if __name__ == "__main__":
   for f in Path(ROOT).rglob("**/*"):
     if not (f.is_file() or f.is_symlink()):
